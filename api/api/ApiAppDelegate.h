cafdebe445017e9ebf0c518b4555086959bedc8e
Paulo McNally <paulomcnally@gmail.com>
2014-03-10 21:49:08 -0600

    init

---
+++ api/api/ApiAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ApiAppDelegate.h
+//  api
+//
+//  Created by Paulo McNally on 3/10/14.
+//  Copyright (c) 2014 McNally Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ api/api/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  api
+//
+//  Created by Paulo McNally on 3/10/14.
+//  Copyright (c) 2014 McNally Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ApiAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ApiAppDelegate class]));
+    }
+}
